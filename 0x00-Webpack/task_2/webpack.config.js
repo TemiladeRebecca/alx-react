d1238f260cdf4d80926d7f20afabafab95bba74e
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-01-29 23:26:03 +0100

    React

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public')
+  },
+  performance: {
+    maxAssetSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            }
+          }
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    modules: [
+      'node_modules'
+    ]
+  }
+};
