dbd6f09ac083fcd816a68239e6d8c5a1926bbc99
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-02-01 17:52:17 +0100

    Install Enzyme

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -6,7 +6,7 @@
 
 // setupTests.js
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-18';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
