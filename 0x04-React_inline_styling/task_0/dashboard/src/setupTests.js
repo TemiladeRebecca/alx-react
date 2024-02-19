70fdc31ecc60b216e9817c963ef2bcff7d050abe
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-02-19 14:30:41 +0100

    Inline styling

--- 0x04-React_inline_styling/task_0/dashboard/src/setupTests.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/setupTests.js
@@ -1,11 +1,10 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-// import '@testing-library/jest-dom';
-
 // setupTests.js
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { TextEncoder, TextDecoder } from 'text-encoding';
 
 configure({ adapter: new Adapter() });
+
+// Make TextEncoder and TextDecoder globally available for tests
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
