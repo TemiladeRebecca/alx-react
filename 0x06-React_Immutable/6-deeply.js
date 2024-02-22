5f0b81d4c9bbec8d69a7e200544db6655c773fd7
Talabi Rebecca <122875790+TemiladeRebecca@users.noreply.github.com>
2024-02-22 01:26:19 +0100

    Nested Merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,5 +1,12 @@
 import { Map } from 'immutable';
 
-export default function mergeDeeplyElements(page1, page2) {
-  return Map(page1).mergeDeepWith(Map(page2));
-}
+const mergeDeeplyElements = (page1, page2) => {
+  const mergedPage = Map(page1).mergeWith((oldVal, newVal) => {
+    if (Map.isMap(oldVal) && Map.isMap(newVal)) {
+      return oldVal.merge(newVal);
+    }
+    return newVal !== undefined ? newVal : oldVal;
+  }, Map(page2));
+
+  return mergedPage;
+};
